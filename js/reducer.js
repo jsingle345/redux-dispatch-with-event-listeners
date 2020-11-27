cd8638ea14f54edc79e19e52854e9e9ea577b60a
jsingle345 <jsingle345@gmail.com>
2020-11-26 19:46:24 -0600

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,44 @@
 // add code snippets from README
+
+let state
+let container = document.querySelector('#container')
+const reducer = (state = {count: 0}, action) => {
+
+    switch(action.type){
+        case "INCREASE_COUNT": 
+            return {count: state.count + 1}
+        case "DECREASE_COUNT":
+            return {count: state.count - 1}
+        default: 
+            return state
+    }
+}
+
+function dispatch(action){
+    state = reducer(state, action)
+    render()
+}
+
+
+function render(){
+    
+
+    container.innerText = state.count
+}
+
+dispatch({type: "lettuce"})
+
+let button = document.querySelector('#button')
+
+button.addEventListener('click', () => {
+    dispatch({type: "INCREASE_COUNT"})
+})
+
+let decrease = document.createElement('button')
+
+decrease.innerText = "decrease"
+decrease.addEventListener('click', () => {
+    dispatch({ type: "DECREASE_COUNT"})
+})
+
+document.body.append(decrease)
